8ca69e24353f5da74c661be30ae64a8af572f673
markbac <109081587+markbac@users.noreply.github.com>
2023-04-11 16:11:34 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,72 +2,59 @@ import os
 import re
 import json
 
-dir_path = "/path/to/files"
+# Set global variables
+global_info = {
+    "author": "John Doe",
+    "document_name": "Merged Document",
+    "document_version": "1.0",
+    "output_directory": "./output/",
+    "protected": True,
+    "compacted": True,
+    "table_of_contents": True,
+    "bookmarks": True
+}
 
-def get_file_data(file_path):
-    # extract data from filename using regex
-    pattern = r"(?P<document_number>\d+)_(?P<document_name>.+)_Cat(?P<category>.+)_Sub(?P<sub_category>.+)_Ver(?P<version>\d+)\.pdf"
-    match = re.match(pattern, os.path.basename(file_path))
-    if match:
-        return {
-            "file_name": os.path.basename(file_path),
-            "document_number": match.group("document_number"),
-            "document_name": match.group("document_name"),
-            "category": match.group("category"),
-            "sub_category": match.group("sub_category"),
-            "version": int(match.group("version")),
-            "merge": True,
-            "watermark_present": False,
-            "page_number_display": False
+default_merge_order = {
+    "Policy": [
+        {
+            "sub_category": "DataProtection",
+            "merge_order": 1
+        },
+        {
+            "sub_category": "InformationSecurity",
+            "merge_order": 2
         }
-    else:
-        return None
+    ],
+    "CodeOfConduct": [
+        {
+            "merge_order": 1
+        }
+    ]
+}
 
-def create_merge_order(files):
-    merge_order = {}
-    for file in files:
-        if file["category"] not in merge_order:
-            merge_order[file["category"]] = [{"sub_category": file["sub_category"], "merge_order": 2}]
-        elif any(d.get("sub_category") == file["sub_category"] for d in merge_order[file["category"]]):
-            pass
-        else:
-            merge_order[file["category"]].append({"sub_category": file["sub_category"], "merge_order": 2})
-    return merge_order
+category_order = [
+    "CodeOfConduct",
+    "Policy"
+]
 
-def main():
-    # get list of all pdf files in directory
-    pdf_files = [os.path.join(dir_path, f) for f in os.listdir(dir_path) if f.endswith(".pdf")]
+# Create an empty list to store the file information
+file_info_list = []
 
-    # get data for each pdf file
-    files_data = [get_file_data(file) for file in pdf_files]
-    files_data = list(filter(None, files_data))
-
-    # create merge order
-    default_merge_order = create_merge_order(files_data)
-    category_order = list(default_merge_order.keys())
-
-    # create dictionary with all data
-    json_data = {
-        "global_values": {
-            "author": "",
-            "document_name": "",
-            "output_directory": "",
-            "document_version": ""
-        },
-        "files": files_data,
-        "options": {
-            "add_table_of_contents": False,
-            "add_bookmarks": False,
-            "output_protected": False,
-            "output_compacted": False
-        },
-        "default_merge_order": default_merge_order,
-        "category_order": category_order
-    }
-
-    # write dictionary to json file
-    with open(os.path.join(dir_path, "mergeFiles.json"), "w") as outfile:
-        json.dump(json_data, outfile, indent=4)
-
-if __name__ == "__main__":
-    main()
+# Define a function to parse the filename and create a dictionary
+def parse_filename(filename):
+    # Use regex to extract information from the filename
+    pattern = r'(?P<category>[\w\s]+)-(?P<sub_category>[\w\s]+)-v(?P<version>[\d\.]+).pdf'
+    match = re.search(pattern, filename)
+    
+    # If regex match found, create dictionary
+    if match:
+        category = match.group('category').strip()
+        sub_category = match.group('sub_category').strip()
+        version = match.group('version').strip()
+        
+        document_name = category + ' ' + sub_category
+        document_number = ''
+        merge_order = None
+        
+        # Check if default merge order exists for category and sub-category
+        if category in default_merge_order:
