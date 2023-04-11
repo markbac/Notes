e6342e941bf89a8cf5059914c18da37113b008f8
markbac <109081587+markbac@users.noreply.github.com>
2023-04-11 16:17:44 +0100

    Update index.py

--- index.py
+++ index.py
@@ -2,14 +2,8 @@ import os
 import re
 import json
 
-# Regular expression pattern to extract document number, name, category, sub-category, and version from filename
-filename_pattern = re.compile(r"^(?P<category>\w+)\s?-?\s?(?P<sub_category>\w*)\s?-?\s?(?P<document_name>.+)\s?-?\s?v?(?P<version>\d+\.\d+)\.pdf$", re.IGNORECASE)
-
-# Directory containing PDF files to be indexed
-pdf_directory = "./pdfs"
-
-# Global document information
-global_info = {
+# Set global configuration options
+config = {
     "author": "John Doe",
     "document_name": "Merged Document",
     "document_version": "1.0",
@@ -20,77 +14,64 @@ global_info = {
     "bookmarks": True
 }
 
-# List to store file information
-file_info = []
+# Define function to parse file name and extract metadata
+def parse_file_name(file_name):
+    metadata = {}
+    match = re.search(r'^(.+?)\s*-\s*(.+?)\s*-\s*v(.+?)\..+?$', file_name)
+    if match:
+        metadata["document_name"] = match.group(1).strip()
+        metadata["category"] = match.group(2).strip()
+        metadata["version"] = match.group(3).strip()
+        match = re.search(r'^(.+?)\s*-\s*(.+?)\s*-\s*v(.+?)\.(.+?)$', file_name)
+        metadata["sub_category"] = match.group(4).strip()
+    else:
+        metadata["file_name"] = file_name
+    return metadata
 
-# Default merge order for each category and sub-category
-default_merge_order = {
-    "Policy": [
-        {
-            "sub_category": "DataProtection",
-            "merge_order": 1
-        },
-        {
-            "sub_category": "InformationSecurity",
-            "merge_order": 2
-        }
-    ],
-    "CodeOfConduct": [
-        {
-            "merge_order": 1
-        }
+# Define function to create dictionary object for a file
+def create_file_dict(file_path):
+    file_dict = {}
+    file_dict["file_name"] = os.path.basename(file_path)
+    file_dict.update(parse_file_name(file_dict["file_name"]))
+    file_dict["document_number"] = "00000"
+    file_dict["merge"] = True
+    file_dict["watermark_present"] = False
+    file_dict["page_numbers"] = True
+    return file_dict
+
+# Index all pdf files in directory and create dictionary objects
+pdf_files = []
+for file_name in os.listdir("."):
+    if file_name.endswith(".pdf"):
+        pdf_files.append(create_file_dict(file_name))
+
+# Create JSON output in desired format
+output = {
+    "global": config,
+    "files": pdf_files,
+    "default_merge_order": {
+        "Policy": [
+            {
+                "sub_category": "DataProtection",
+                "merge_order": 1
+            },
+            {
+                "sub_category": "InformationSecurity",
+                "merge_order": 2
+            }
+        ],
+        "CodeOfConduct": [
+            {
+                "merge_order": 1
+            }
+        ]
+    },
+    "category_order": [
+        "CodeOfConduct",
+        "Policy"
     ]
 }
 
-# Order in which to merge each category
-category_order = [
-    "CodeOfConduct",
-    "Policy"
-]
-
-# Loop through PDF files in directory
-for filename in os.listdir(pdf_directory):
-    if not filename.endswith(".pdf"):
-        continue
-        
-    # Extract document information from filename using regular expression
-    match = filename_pattern.match(filename)
-    if match:
-        document_number = ""
-        document_name = match.group("document_name")
-        category = match.group("category")
-        sub_category = match.group("sub_category")
-        version = match.group("version")
-    else:
-        document_number = ""
-        document_name = ""
-        category = ""
-        sub_category = ""
-        version = ""
-        
-    # Create dictionary for file information
-    file_dict = {
-        "file_name": filename,
-        "document_number": document_number,
-        "document_name": document_name,
-        "category": category,
-        "sub_category": sub_category,
-        "version": version,
-        "merge": True,
-        "watermark_present": False,
-        "page_numbers": True
-    }
-    
-    # Add file dictionary to list
-    file_info.append(file_dict)
-
-# Create dictionary to store all information
-merged_dict = {
-    "global": global_info,
-    "files": file_info,
-    "default_merge_order": default_merge_order,
-    "category_order": category_order
-}
-
-# Output dictionary as JSON
-print(json.dumps(merged_dict, indent=4))
+# Output JSON to file
+with open("output.json", "w") as f:
+    json.dump(output, f, indent=2)
