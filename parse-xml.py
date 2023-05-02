3d4e198ec315484110f416370c39065cc4b637aa
markbac <109081587+markbac@users.noreply.github.com>
2023-05-02 07:45:02 +0100

    Update parse-xml.py

--- parse-xml.py
+++ parse-xml.py
@@ -1,6 +1,5 @@
-import ast
 import xml.etree.ElementTree as ET
-
+import csv
 import logging
 import coloredlogs
 import sys
@@ -12,6 +11,18 @@ logger = logging.getLogger(name='mylogger')
 
 coloredlogs.install(logger=logger)
 logger.propagate = False
+
+def duration_to_seconds(duration):
+    components = duration[2:].split('T')  # split duration into date and time components
+
+    
+    time_components = components[0] if len(components) >= 1 else ''  # extract time component
+    days = components[0].split('D')[0] if 'D' in components[0] else '0'  # extract days component
+    hours = time_components.split('H')[0] if 'H' in time_components else '0'  # extract hours component
+    minutes = time_components.split('M')[0].split('H')[-1] if 'M' in time_components else '0'  # extract minutes component
+    seconds = time_components.split('S')[0].split('M')[-1] if 'S' in time_components else '0'  # extract seconds component
+    total_seconds = (int(days) * 86400) + (int(hours) * 3600) + (int(minutes) * 60) + int(seconds)  # calculate total seconds
+    return total_seconds
 
 
 coloredFormatter = coloredlogs.ColoredFormatter(
@@ -46,7 +57,7 @@ tree = None
 
 pp = pprint.PrettyPrinter(indent=4)
 
-xmlfile = "CEM-updatereport-ForecastProfileIntendedOperations.xml"
+xmlfile = "oaddr2.xml"
 
 #open the file
 fileptr = open(xmlfile,"r")
@@ -63,21 +74,21 @@ oadr_dict=xmltodict.parse(xml_content)
 #Use contents of ordered dict to make python dictionary
 oadrPayload= dict(oadr_dict['oadr:oadrPayload'])
 
-
-
 oadrSignedObject = dict(oadrPayload['oadr:oadrSignedObject'])
 
 
 
-oadrRegisterReport = dict(oadrSignedObject['oadr:oadrRegisterReport'])
+if 'oadr:oadrRegisterReport' in oadr_dict['oadr:oadrPayload']['oadr:oadrSignedObject']:
+    oadrReport = dict(oadr_dict['oadr:oadrPayload']['oadr:oadrSignedObject']['oadr:oadrRegisterReport']['oadr:oadrReport'])
 
 
-oadrReport = dict(oadr_dict['oadr:oadrPayload']['oadr:oadrSignedObject']['oadr:oadrRegisterReport']['oadr:oadrReport'])
+if 'oadr:oadrUpdateReport' in oadr_dict['oadr:oadrPayload']['oadr:oadrSignedObject']:
+    oadrReport = dict(oadr_dict['oadr:oadrPayload']['oadr:oadrSignedObject']['oadr:oadrUpdateReport']['oadr:oadrReport'])
 
-logger.debug ("xcal:date-time %s ", oadrReport['xcal:dstart']['xcal:date-time'])
+pp.pprint(oadrReport['xcal:dtstart'])
 
 
-#pp.pprint(interval)
+intervalStart = oadrReport['xcal:dtstart']['xcal:date-time']
 
 eiReportID = oadrReport['ei:eiReportID']
 reportRequestID = oadrReport['ei:reportRequestID']
@@ -85,26 +96,37 @@ reportSpecifierID = oadrReport['ei:reportSpecifierID']
 reportName = oadrReport['ei:reportName']
 createdDateTime = oadrReport['ei:createdDateTime']
 
-logger.debug("eiReportID %s",eiReportID)
-logger.debug("reportRequestID %s",reportRequestID)
-logger.debug("reportSpecifierID %s",reportSpecifierID)
-logger.debug("reportName %s",reportName)
-logger.debug("createdDateTime %s",createdDateTime)
+logger.debug (eiReportID)
 
-count = 1
-for eiinterval in oadrReport['strm:intervals']['strm:interval']['ei:interval']:
-    #pp.pprint(eiinterval)
-    logger.debug("interval %d",count)
-    
-    if "xcal:dstart" in eiinterval:
-        intervalStart = eiinterval['xcal:dstart']['xcal:date-time']
-        logger.debug("\t intervalStart %s",intervalStart)
-    
-    duration = eiinterval['xcal:duration']['xcal:duration']
-    logger.debug("\t duration %s",duration)
-    
-    oadrReportPayload =  dict(eiinterval['oadr:oadrReportPayload'])
-    power = eiinterval['oadr:oadrReportPayload']['ei:payloadFloat']['ei:value']
-    logger.debug("\t power %s",power)
-    
-    count += 1
+
+filename = eiReportID.replace(":","-").replace("; ","_")
+
+
+print(filename) # Output: "order-LD_frc-1_intervals-4_esa_id-ESA#1"
+
+with open(f"{filename}.csv", mode='w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(["start_time", "duration", "power", "interruptible"])
+
+    count = 1
+    for eiinterval in oadrReport['strm:intervals']['ei:interval']:
+        #pp.pprint(eiinterval)
+        interruptible = 'true'
+        logger.debug("interval %d",count)
+        
+        if "xcal:dstart" in eiinterval:
+            intervalStart = eiinterval['xcal:dstart']['xcal:date-time']
+            logger.debug("\t intervalStart %s",intervalStart)
+        
+        duration = eiinterval['xcal:duration']['xcal:duration']
+        duration = duration_to_seconds(duration)
+        logger.debug("\t duration %s",duration)
+        
+        oadrReportPayload =  dict(eiinterval['oadr:oadrReportPayload'])
+        power = eiinterval['oadr:oadrReportPayload']['ei:payloadFloat']['ei:value']
+        logger.debug("\t power %s",power)
+        
+        writer.writerow([intervalStart, duration, power, interruptible])
+
+        count += 1
+        intervalStart = ''
